3641efbdd88bd6c4b0768371dbef4fc190f90865
Bimal-Prakash <bimalpb123@gmail.com>
2026-08-05 09:30:45 +0530

    feat: implement LiquidIsland core widget with module management and dynamic UI state animations

--- core/island.py
+++ core/island.py
@@ -16,7 +16,8 @@ class LiquidIsland(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.Tool)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
+        self.setWindowTitle("LiquidIsland")
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         
         self.current_state = "idle"
